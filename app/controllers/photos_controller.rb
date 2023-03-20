5060f0db7bfcf1156fb41c632a970bb399380184
Tan Sakeer <tansakeer@gmail.com>
2023-03-20 16:05:10 +0000

    completed delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,10 +9,10 @@ class PhotosController < ActionController::Base
 
   def photo_details
 
-    uname = params.fetch("ph")
-    matching_usernames= Photo.where({ :username => uname})
+    uid = params.fetch("ph")
+    matching_photos= Photo.where({ :id=> uid})
 
-    @the_photo = matching_usernames.at(0)
+    @the_photo = matching_photos.at(0)
 
     if @the_photo == nil
       redirect_to("/404")
@@ -21,4 +21,15 @@ class PhotosController < ActionController::Base
     end
   end
 
+  def del_photo
+    the_id = params.fetch("del_ph")
+    matching_photos= Photo.where({ :id=> the_id})
+
+    @the_photo = matching_photos.at(0)
+    @the_photo.destroy 
+
+    # @the_photo
+    #render({ :template => "photo_templates/del_photo_details.html.erb" })
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ get("/users/:name", { :controller => "users", :action => "user_details"})
 
 get("/photos", { :controller => "photos", :action => "photo_index"})
 get("/photos/:ph", { :controller => "photos", :action => "photo_details"})
+get("delete_photo/:del_ph", { :controller => "photos", :action => "del_photo"})
 
 end
